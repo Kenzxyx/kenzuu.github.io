b84c042ace55f352b2d4ec33e9c72d3fe6d322d7
kenzu <148296628+Kenzxyx@users.noreply.github.com>
2023-10-25 12:10:36 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,22 @@
+let nilai = prompt("masukan nilai anda",0);
+
+if(nilai <= 66){
+    document.write("F-")
+}else if (nilai <=75){
+    document.write("F")
+}else if (nilai <=80){
+    document.write("E")
+}else if (nilai <=85){
+    document.write("D")
+}else if (nilai <=90){
+    document.write("C")
+}else if (nilai <=95){   
+    document.write("B")
+}else if (nilai <=99){
+    document.write("A")
+}
+
+else (nilai < 0 || nilai > 99)
+document.write("error")
+
+
